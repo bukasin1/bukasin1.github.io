dc35043c7613e8ee511c9a6c747940d90ae46bfd
Ebuka <bukasin1@gmail.com>
2021-07-03 14:13:12 +0100

    Js Update

--- star-wars/script/index.js
+++ star-wars/script/index.js
@@ -1,25 +1,53 @@
 function main(){
 
 }
-let images = [
-    "./images/luke-Sky.jpeg",
-    "./images/c3-po.jpeg",
-    "./images/r2-d2.jpeg",
-    "./images/Anakin_Skywalker_RotS.png",
-    "./images/princess-leia-1200.jpeg",
-    "./images/OwenCardTrader.png",
-    "./images/beru-lars.jpeg" ,
-    "./images/r5-d4.jpeg",
-    "./images/BiggsHS-ANH.png",
-    "./images/obi-wan-ken.jpeg"
-]
-let names = document.getElementsByClassName('name')
-let copulateData = []
-let name = document.getElementById('modalName')
-let gender = document.getElementById('gender')
-let height = document.getElementById('height')
+
+// Images data
+    let images = [
+        "./images/luke-Sky.jpeg",
+        "./images/c3-po.jpeg",
+        "./images/r2-d2.jpeg",
+        "./images/Anakin_Skywalker_RotS.png",
+        "./images/princess-leia-1200.jpeg",
+        "./images/OwenCardTrader.png",
+        "./images/beru-lars.jpeg" ,
+        "./images/r5-d4.jpeg",
+        "./images/BiggsHS-ANH.png",
+        "./images/obi-wan-ken.jpeg"
+    ]
+//let populateData = []
+
+// Modal contents
+    let name = document.getElementById('modalName')
+    let gender = document.getElementById('gender')
+    let height = document.getElementById('height')
+
+// Handling the modal
+    const modal = document.getElementById("myModal");
+
+    // Get the <span> element that closes the modal
+    const span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+        modal.style.display = "none";
+        }
+    }
+
 let mainBlock = document.getElementById('main')
+
+// Api URL
 const starWarsUrl = "https://swapi.dev/api/people"
+
+//Fetch and handle api data
 fetch(starWarsUrl).then(resp => resp.json()).then(data => {
 
     let charactersContainer = document.createElement('div')
@@ -50,26 +78,5 @@ fetch(starWarsUrl).then(resp => resp.json()).then(data => {
 
 
 })
-
-const modal = document.getElementById("myModal");
-
-
-
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 
 //module.exports = {main}
